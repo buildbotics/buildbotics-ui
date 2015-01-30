3c656c2dbb492fce906b69b2360b9ed839e550f5
Joseph Coffland <joseph@cauldrondevelopment.com>
2015-01-29 16:27:46 -0800

    Show spinner for loading images in things view

--- src/js/things.js
+++ src/js/things.js
@@ -5,4 +5,26 @@ module.exports = {
   replace: true,
   template: '#things-template',
   paramAttributes: ['things'],
+
+
+  watch: {
+    things: function () {
+      var element = $(this.$el);
+
+      // Hide images before they are loaded
+      var images = element.find('img');
+      images.each(function () {
+        if (!this.complete) $(this).hide();
+      });
+
+      // Unhide after load
+      images.one('load', function () {
+        $(this).fadeIn(500);
+
+      }).each(function () {
+        // Make sure we get the load signal even for cached images
+        if (this.complete) $(this).load()
+      });
+    }
+  }
 }
